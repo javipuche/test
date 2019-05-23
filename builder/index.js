aeb93b77977d582f8194ba115966a926078ce092
Javier Puche <j.puche@avoristravel.com>
2019-05-23 15:34:11 +0200

    tests

--- builder/index.js
+++ builder/index.js
@@ -1,17 +1,12 @@
-// import gulp from 'gulp'
-// import cleanDist from './tasks/cleanDist'
-// import fonts from './tasks/fonts'
-// import images from './tasks/images'
-// import staticFolder from './tasks/staticFolder'
-// import webpackAssets from './tasks/webpackAssets'
-// import cleanEmptyFolders from './tasks/cleanEmptyFolders'
-// import cleanTmp from './tasks/cleanTmp'
-//
-// gulp.task('commons', gulp.series(cleanDist, gulp.parallel(fonts, images, staticFolder), webpackAssets, cleanEmptyFolders, cleanTmp))
-// gulp.task('build', gulp.series('commons'))
-// gulp.task('default', gulp.series('commons'))
-const gulp = require('gulp')
-const builder = require('../../maquetus/maquetus-builder/index')
-const config = require('./config')
+import gulp from 'gulp'
+import cleanDist from './tasks/cleanDist'
+import fonts from './tasks/fonts'
+import images from './tasks/images'
+import staticFolder from './tasks/staticFolder'
+import webpackAssets from './tasks/webpackAssets'
+import cleanEmptyFolders from './tasks/cleanEmptyFolders'
+import cleanTmp from './tasks/cleanTmp'
 
-builder(gulp, config)
+gulp.task('commons', gulp.series(cleanDist, gulp.parallel(fonts, images, staticFolder), webpackAssets, cleanEmptyFolders, cleanTmp))
+gulp.task('build', gulp.series('commons'))
+gulp.task('default', gulp.series('commons'))
